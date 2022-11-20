3deb934b57b863586d9daeca68bf8c6b5fa61d90
proxy0001 <g.proxy0001@gmail.com>
2022-11-20 23:19:33 +0800

    optimized parser and test cases

--- test/parser.test.js
+++ test/parser.test.js
@@ -2,34 +2,72 @@ const parser = require("../utils/parser.js");
 const expect = require("chai").expect;
 
 // a test suite for parser
-describe('support single lines', function () {
+describe('support one line', function () {
     it('should return [] when the input is empty', function () {
         expect(parser('')).to.eql([])
     })
 
-    it('should return words as title, numbers as amount when the input is not empty', function () {
+    it('has empty title and 1 item, should return empty title and 1 item', function () {
         const [title, items, amount, createDatetime] = parser('hello 100');
-        expect(title).to.equal('hello');
+        expect(title).to.equal('');
+        expect(items).to.equal('hello 100');
         expect(amount).to.equal(100);
     })
 
-    it('should return any charachters as title, numbers in the end as amount when the title has numbers', function () {
-        const [title, items, amount, createDatetime] = parser('hel23 lo 100');
-        expect(title).to.equal('hel23 lo');
+    it('has empty title and 1 item which has no space between item name and amount, should return empty title and 1 item', function () {
+        const [title, items, amount, createDatetime] = parser('hello100');
+        expect(title).to.equal('');
+        expect(items).to.equal('hello 100');
         expect(amount).to.equal(100);
     })
 
-    it('should return any charachters as title, numbers in the end as amount even without space between', function () {
-        const [title, items, amount, createDatetime] = parser('hel23 lo100');
-        expect(title).to.equal('hel23 lo');
+    it('has empty title and 2 item one has space between item name and amount and another is not, return title and 2 items, sum of amount of each item', function () {
+        const [title, items, amount, createDatetime] = parser('goods 100 desk1000');
+        expect(title).to.equal('');
+        expect(items).to.equal('goods 100\ndesk 1000');
+        expect(amount).to.equal(1100);
+    })
+
+    it('has title and 1 item, should return title and 1 item', function () {
+        const [title, items, amount, createDatetime] = parser('shopname goods 100');
+        expect(title).to.equal('shopname');
+        expect(items).to.equal('goods 100');
         expect(amount).to.equal(100);
     })
 
-    it('should separate title and items correctly and amount is the sum of items', function () {
-        const [title, items, amount, createDatetime] = parser('adsf11sdf    adfasdf 12 adfadf 12 asdfasdf 2122');
-        expect(title).to.equal('adsf11sdf');
-        expect(items).to.equal(`adfasdf 12\nadfadf 12\nasdfasdf 2122`);
-        expect(amount).to.equal(2146);
+    it('has title and 1 item which has no space between item name and amount, should return title and 1 item', function () {
+        const [title, items, amount, createDatetime] = parser('shopname goods100');
+        expect(title).to.equal('shopname');
+        expect(items).to.equal('goods 100');
+        expect(amount).to.equal(100);
+    })
+
+    it('has title and 2 items all have space between item name and amount, return title and 2 items, sum of amount of each item', function () {
+        const [title, items, amount, createDatetime] = parser('shopname goods 100 desk 1000');
+        expect(title).to.equal('shopname');
+        expect(items).to.equal('goods 100\ndesk 1000');
+        expect(amount).to.equal(1100);
+    })
+
+    it('has title and 2 items all have no space between item name and amount, return title and 2 items, sum of amount of each item', function () {
+        const [title, items, amount, createDatetime] = parser('shopname goods100 desk1000');
+        expect(title).to.equal('shopname');
+        expect(items).to.equal('goods 100\ndesk 1000');
+        expect(amount).to.equal(1100);
+    })
+
+    it('has title and 2 items one has space between item name and amount and another is not, return title and 2 items, sum of amount of each item', function () {
+        const [title, items, amount, createDatetime] = parser('shopname goods 100 desk1000');
+        expect(title).to.equal('shopname');
+        expect(items).to.equal('goods 100\ndesk 1000');
+        expect(amount).to.equal(1100);
+    })
+
+    it('has title and 2 items, one item name has number at the end of name, return title and 2 items, sum of amount of each item', function () {
+        const [title, items, amount, createDatetime] = parser('這是店家 東西 12 東西2 12');
+        expect(title).to.equal('這是店家');
+        expect(items).to.equal(`東西 12\n東西2 12`);
+        expect(amount).to.equal(24);
     })
 
 });
--- utils/parser.js
+++ utils/parser.js
@@ -18,11 +18,12 @@ const processor = (function () {
         return []
     };
     const extractor = function (text, regExp, titleHandler, collectionHandler) {
-        const matches = text.trim().matchAll(regExp);
-        if (!matches) return illegal();
+        const matches = [...text.trim().matchAll(regExp)];
+        if (!matches.length) return illegal();
+        if (matches[0]['index'] !== 0) return illegal();
         let title = '', collection = [];
         for (const [index, match] of [...matches].entries()) {
-            trimedMatch = match.map(x => x && x.trim instanceof Function ? x.trim() : x)
+            trimedMatch = match.map(x => x && typeof x.trim === 'function' ? x.trim() : x)
             title = titleHandler(index, trimedMatch, title)
             collection = collectionHandler(index, trimedMatch, collection)
         }
@@ -31,19 +32,26 @@ const processor = (function () {
         const createDatetime = formatDatetime(getLocalTime());
         return [title, items, amount, createDatetime];
     };
-    const singleLine = function (text) {
-        return extractor(text, /^([^ ]*) +(\D*) *(\d+)|(\D*) *(\d+)/g, (index, match, title) => {
-            return index === 0 && match[1] ? match[1].trim() : title
+    const oneLineWithEmptyTitle = function (text) {
+        return extractor(text, /(\D+)(\d+)/gm, (index, match, title) => '', (index, match, collection) => {
+            if (match[2]) collection.push([match[1], match[2]])
+            return collection
+        })
+    };
+    const oneLine = function (text) {
+        return extractor(text, /^([^ ]*) +([^ ]*\D)(\d+)| +([^ ]*) +(\d+)| +(\D+)(\d+)/gm, (index, match, title) => {
+            return index === 0 && match[1] ? match[1] : title
         }, (index, match, collection) => {
             if (index !== 0 && match[1]) collection.push([match[1], ''])
             if (match[3]) collection.push([match[2], match[3]])
             if (match[5]) collection.push([match[4], match[5]])
+            if (match[7]) collection.push([match[6], match[7]])
             return collection
         })
     };
     const multiLines = function (text) {
         return extractor(text, /(.+\D)(\d+) *$|(.*[\D^ *]) *$/gm, (index, match, title) => {
-            return index === 0 && match[3] ? match[3].trim() : title
+            return index === 0 && match[3] ? match[3] : title
         }, (index, match, collection) => {
             if (index !== 0 && match[3]) collection.push([match[3], ''])
             if (match[2]) collection.push([match[1], match[2]])
@@ -52,19 +60,35 @@ const processor = (function () {
     };
     const executions = {
         illegal,
-        singleLine,
+        oneLineWithEmptyTitle,
+        oneLine,
         multiLines,
     };
+    const exec = function (name) {
+        if (!(executions[name] instanceof Function)) throw new Error('There is no corresponding execution method for the plan.')
+        return executions[name]
+    }
+    const iterativelyExec = function (plan) {
+        return (text) => {
+            for (let i = 0;i < plan.length; i++) {
+                const res = exec(plan[i])(text);
+                if (!res.length) continue;
+                else return res;
+            }
+            return exec('illegal')(text);
+        }
+    }
     return function (plan) {
-        if (!(executions[plan] instanceof Function)) throw new Error('There is no corresponding execution method for the plan.')
-        return executions[plan]
+        if (typeof plan === 'string') return exec(plan)
+        if (Array.isArray(plan)) return iterativelyExec(plan)
+        throw new Error('type error of plan, which accept String and Array')
     };
 })();
 
 const planner = function (text) {
     if (!text) return 'illegal';
     if (/\r|\n/.exec(text)) return 'multiLines';
-    else return 'singleLine';
+    else return ['oneLine', 'oneLineWithEmptyTitle'];
 }
 
 const parser = function (text) {
