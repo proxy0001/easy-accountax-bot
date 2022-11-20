a2e55774bbe199104f13d38d7b1cff5754219438
proxy0001 <g.proxy0001@gmail.com>
2022-11-20 16:44:22 +0800

    modify structure to support multi format of input

--- utils/parser.js
+++ utils/parser.js
@@ -1,23 +1,55 @@
-function parser (text) {
-    if (!text) return [];
-    const matched = text.match(/(.+\D)(\d+)$/);
-    if (!matched) return [];
-    const title = matched[1].trim();
-    const amount = parseInt(matched[2]);
-    const createDatetime = formatDatetime(getLocalTime());
-    const items = ''
-    return [title, items, amount, createDatetime]
-}
-
-function getLocalTime () {
+const getLocalTime = function () {
     const now = new Date();
     const offsetMs = now.getTimezoneOffset() * 60 * 1000;
     const datetimeLocal = new Date(now.getTime() - offsetMs);
-    return datetimeLocal
-}
+    return datetimeLocal;
+};
 
-function formatDatetime (datetime) {
+const formatDatetime = function (datetime) {
     return datetime.toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
+};
+
+const processor = (function () {
+    const illegal = function (text) {
+        return []
+    };
+    const singleLine = function (text) {
+        const matched = text.match(/(.+\D)(\d+)$/);
+        if (!matched) return illegal();
+        const title = matched[1].trim();
+        const amount = parseInt(matched[2]);
+        const createDatetime = formatDatetime(getLocalTime());
+        const items = '';
+        return [title, items, amount, createDatetime];
+    };
+    const multiLines = function (text) {
+        const matched = text.match(/(.+\D)(\d+)$/);
+        if (!matched) return illegal();
+        const title = matched[1].trim();
+        const amount = parseInt(matched[2]);
+        const createDatetime = formatDatetime(getLocalTime());
+        const items = '';
+        return [title, items, amount, createDatetime];
+    };
+    const executions = {
+        illegal,
+        singleLine,
+        multiLines,
+    };
+    return function (plan) {
+        if (!(executions[plan] instanceof Function)) throw new Error('There is no corresponding execution method for the plan.')
+        return executions[plan]
+    };
+})();
+
+const planner = function (text) {
+    if (!text) return 'illegal';
+    if (/\r|\n/.exec(text)) return 'multiLines';
+    else return 'singleLine';
 }
 
-module.exports = parser
+const parser = function (text) {
+    return processor(planner(text))(text)
+}
+
+module.exports = parser;
